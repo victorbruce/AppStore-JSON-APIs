ebeeb5a379e5a870e6d08e84bf9b66f71800980f
victorbruce <victorbruce82@gmail.com>
2023-07-10 16:18:00 +0000

    render collection view cells in search view controller

--- AppleStoreJsonApi/Controllers/SearchViewController.swift
+++ AppleStoreJsonApi/Controllers/SearchViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class SearchViewController: UICollectionViewController  {
+class SearchViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout  {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,6 +15,21 @@ class SearchViewController: UICollectionViewController  {
         
         navigationItem.title = "Search"
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        collectionView.register(SearchResultCell.self, forCellWithReuseIdentifier: SearchResultCell.identifier)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 350)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SearchResultCell.identifier, for: indexPath) as? SearchResultCell else { return UICollectionViewCell() }
+        return cell
     }
 
     init() {
