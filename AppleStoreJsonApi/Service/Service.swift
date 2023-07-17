e0a1dc1c6fa16155345fdce55322a93e0fae3bdb
victorbruce <victorbruce82@gmail.com>
2023-07-17 11:55:14 +0000

    create a service layer to handle network calls

---
+++ AppleStoreJsonApi/Service/Service.swift
@@ -0,0 +1,37 @@
+//
+//  Service.swift
+//  AppleStoreJsonApi
+//
+//  Created by Victor Bruce on 17/07/2023.
+//
+
+import Foundation
+
+class Service {
+    static let shared = Service() // singleton object
+    
+    func fetchApps(completion: @escaping ([Result], Error?) -> ()) {
+        let urlString = "https://itunes.apple.com/search?term=instagram&entity=software"
+        guard let url = URL(string: urlString) else {return}
+
+        // fetch data from the internet
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("Failed to fetch apps:", error)
+                completion([], nil)
+                return;
+            }
+            // success
+            guard let data = data else { return }
+
+            do {
+                let searchResult = try JSONDecoder().decode(SearchResult.self, from: data)
+                completion(searchResult.results, nil)
+            } catch let jsonError {
+                print("Failed to decode data", jsonError)
+                completion([], jsonError)
+            }
+
+        }.resume() // fires off the request
+    }
+}
