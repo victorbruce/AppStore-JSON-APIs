06f3bcf770fd1656d6004bb582cc9adcfbc56cf0
victorbruce <victorbruce82@gmail.com>
2023-07-17 09:08:42 +0000

    create a search result model to present api response data in the app

---
+++ AppleStoreJsonApi/Model/SearchResult.swift
@@ -0,0 +1,18 @@
+//
+//  SearchResult.swift
+//  AppleStoreJsonApi
+//
+//  Created by Victor Bruce on 17/07/2023.
+//
+
+import Foundation
+
+struct SearchResult: Decodable {
+    let resultCount: Int
+    let results: [Result]
+}
+
+struct Result: Decodable {
+    let trackName: String
+    let primaryGenreName: String
+}
